5bdc050fcc8f90802cbe4a05a43b9c99a5b653b4
George Warner <georgewarner1998@gmail.com>
2017-09-22 13:27:55 +0100

    Completed Exercise5.9.2

--- Exercise5.9.2/WorkFuture.java
+++ Exercise5.9.2/WorkFuture.java
@@ -11,11 +11,32 @@ public class WorkFuture
     //year they will retire
     int retirementYear = birthYear + 68;
 
+    //years left Working
+    int yearsToWork = retirementYear - presentYear;
+
     //State the year you were born in
     System.out.println("you were born in " + birthYear);
 
-    //The year user will retire
-    System.out.println("you will retire in " + retirementYear);
+    //state years left Working
+    System.out.println("you have " + yearsToWork + " years left to work");
 
+    //year after birth
+    int someYear = retirementYear;
+    yearsToWork = someYear - retirementYear;
+
+    //while
+    while (someYear != presentYear)
+    {
+      if (yearsToWork == 0)
+        System.out.println("you will retire in " + someYear);
+      else
+      {
+        //print out years left Working
+        System.out.println("in " + someYear + " you will have " + yearsToWork + " years left working");
+      }
+      someYear = someYear - 1;
+      yearsToWork = retirementYear - someYear;
+
+    }//while
   }
 }
